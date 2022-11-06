5d0db98a95cc91c16c6869bb57c1212f6101db42
Aiwendil <adriencallico@hotmail.fr>
2022-11-06 19:14:53 +0100

    rapport + condition angles départ/arrivée pour mode 1

---
+++ todo rapport.py
@@ -0,0 +1,11 @@
+
+
+#commentaires ok, s'assurer que tout se déroule bien
+#print les distances des points lors de l'assertion de la calibration
+
+#rapport :
+
+#dire où trouver quoi / lancer le main
+#calibration : quels angles + erreur + résultats / quels points pour l'assertion
+#path following : mettre en valeur le cercle déformé
+#path planning : expliquer équations obstacles / problème des singularités / filtrage des chemins / det jacobienne
